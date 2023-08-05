f468a98ce0ff1a2aa2c5e1353c6cd8f2e9ec079c
Andrey199123 <118031542+Andrey199123@users.noreply.github.com>
2023-08-05 00:00:29 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,28 @@
-import java.nio.file.FileStore;
-import java.nio.file.FileSystems;
-
-public class Main {
+//Andrey Vasilyev, June 30th, 2023
+package Class_File;
+import java.io.*;
+import java.nio.file.*;
+import java.util.stream.IntStream;
+public class file_creation {
     public static void main(String[] args) {
-        for (FileStore store: FileSystems.getDefault().getFileStores()) {
-            System.out.println(store);
-        }
+        final long startTime = System.nanoTime();
+        IntStream.rangeClosed(0, 1000).forEach(i -> {
+            try {
+                new File("/Users/yuriyvasiliev/Downloads/hello" + i + ".txt").createNewFile();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        IntStream.rangeClosed(0, 1000).forEach(i -> {
+            try {
+                Files.delete(Paths.get("/Users/yuriyvasiliev/Downloads/hello" + i + ".txt"));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        System.out.println("Completed in " + (System.nanoTime() - startTime)/1000000 + " milliseconds.");
     }
 }
+//534, 622, 539 for txt
+//729, 519, 455, 523 for jpg
+//764, 505, 569, 468 for png
